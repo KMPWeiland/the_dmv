beb59992f6e4f2ddea55d967bd42d894c102cd2f
Kristin Weiland <kristinweiland@gmail.com>
2024-10-19 07:53:16 -0700

    Add functionality to Facility class, including adding vehicle registration date.

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,26 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services,
+              :registered_vehicles
 
   def initialize(facility_details)
     @name = facility_details[:name] 
     @address = facility_details[:address]
     @phone = facility_details[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
   def add_service(service)
     @services << service
   end
+
+  def update_registered_vehicles(vehicle)
+    @registered_vehicles << vehicle
+  end
 end
+
+# methods to create based on interaction pattern:
+#
