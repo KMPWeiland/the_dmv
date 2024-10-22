6c7c651b88c3ea134ff7319996284398a068af43
Kristin Weiland <kristinweiland@gmail.com>
2024-10-22 14:48:12 -0700

    Refactor code to accommodate new.ny_dmv_office_locations data.

--- lib/dmv_directory.rb
+++ lib/dmv_directory.rb
@@ -1,17 +1,15 @@
 class DmvDirectory
-    # attr_reader :vehicle_data_array,
-    #             :plate_type,
-    #             :registration_date
-
+  
      def create_facilities(dmv_directory_array)
         dmv_directory_array.map do |dmv_directory_hash|
             Facility.new({
-                name: dmv_directory_hash[:dmv_office], 
-                address: dmv_directory_hash[:address_li], #how to to combine addresses in dataset?!
-                phone: dmv_directory_hash[:phone], 
-                services: dmv_directory_hash[:services_p], 
-                registered_vehicles: [],
-                collected_fees: 0
+                name: dmv_directory_hash[:"dmv_office"] || dmv_directory_hash[:"office_name"] || dmv_directory_hash[:"name"], 
+                address: dmv_directory_hash[:"address_li"] || dmv_directory_hash[:"street_address_line_1"] || dmv_directory_hash[:"address1"], 
+                #how to to combine addresses on multiples lines from dataset?!
+                phone: dmv_directory_hash[:phone] || dmv_directory_hash[:"public_phone_number"], 
+                services: dmv_directory_hash[:services_p] || "Unknown" 
+                # registered_vehicles: [],
+                # collected_fees: 0
             })
         end
     end
